421aa4c0de7e3c54b93bc07a681bc3f092ecac75
Andrew Jordan <andrew.k.jordan@gmail.com>
2018-10-18 09:39:55 -0700

    adds Breakout to DHT example

--- examples/Sensors/TemperatureAndHumidity/TemperatureAndHumidity.ino
+++ examples/Sensors/TemperatureAndHumidity/TemperatureAndHumidity.ino
@@ -1,125 +1,75 @@
+#include <DHT.h>
+
 #include <board.h>
 #include <BreakoutSDK.h>
+#include <stdio.h>
+//https://github.com/Seeed-Studio/Grove_Temperature_And_Humidity_Sensor
+#include <DHT.h>
 
-#define SENSOR_PIN    (38)
+/** Change this to your device purpose */
+static const char *device_purpose = "Dev-Kit";
+/** Change this to your key for the SIM card inserted in this device 
+ *  You can find your PSK under the Breakout SDK tab of your Narrowband SIM detail at
+ *  https://www.twilio.com/console/wireless/sims
+*/
+static const char *psk_key = "00112233445566778899aabbccddeeff";
 
-void setup()
-{
-  TemperatureAndHumidityBegin(SENSOR_PIN);
+/** This is the Breakout SDK top API */
+Breakout *breakout = &Breakout::getInstance();
+
+#define SENSOR_PIN (D38)
+#define INTERVAL   (10000)
+#define DHTTYPE DHT11   // DHT 11 
+
+DHT dht(SENSOR_PIN, DHTTYPE);
+
+void setup() {
+  dht.begin();
+  // Feel free to change the log verbosity. E.g. from most critical to most verbose:
+  //   - errors: L_ALERT, L_CRIT, L_ERR, L_ISSUE
+  //   - warnings: L_WARN, L_NOTICE
+  //   - information & debug: L_INFO, L_DB, L_DBG, L_MEM
+  // When logging, the additional L_CLI level ensure that the output will always be visible, no matter the set level.
+  owl_log_set_level(L_INFO);
+  LOG(L_WARN, "Arduino setup() starting up\r\n");
+  
+  // Set the Breakout SDK parameters
+  breakout->setPurpose(device_purpose);
+  breakout->setPSKKey(psk_key);
+  breakout->setPollingInterval(10 * 60);  // Optional, by default set to 10 minutes
+  
+  // Powering the modem and starting up the SDK
+  LOG(L_WARN, "Powering on module and registering...");
+  breakout->powerModuleOn();
+  
+  LOG(L_WARN, "... done powering on and registering.\r\n");
+  LOG(L_WARN, "Arduino loop() starting up\r\n");
+}
+
+/**
+ * This is just a simple example to send a command and write out the status to the console.
+ */
+ 
+void sendCommand(const char * command) {
+  if (breakout->sendTextCommand(command) == COMMAND_STATUS_OK) {
+    LOG(L_INFO, "Tx-Command [%s]\r\n", command);
+  } else {
+    LOG(L_INFO, "Tx-Command ERROR\r\n");
+  }
 }
 
 void loop()
 {
-  float temp;
-  float humi;
-
-  if (!TemperatureAndHumidityRead(&temp, &humi)) {
-    SerialUSB.println("ERROR!");
-    delay(2000);
-  }
-
-  SerialUSB.print("Current humidity = ");
-  SerialUSB.print(humi);
-  SerialUSB.print("%  ");
-  SerialUSB.print("temperature = ");
-  SerialUSB.print(temp);
-  SerialUSB.println("C");
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
   
-  delay(2000);
+  LOG(L_INFO, "Current temperature [%f] degrees celcius\r\n", temperature);
+  LOG(L_INFO, "Current humidity [%f]\r\n", humidity);
+  char commandText[512];
+  snprintf(commandText, 512, "Current humidity [%4.2f] and current temp [%4.2f]", temperature, humidity);
+  sendCommand(commandText);
+
+  breakout->spin();
+
+  delay(INTERVAL);
 }
-
-////////////////////////////////////////////////////////////////////////////////////////
-//
-
-int TemperatureAndHumidityPin;
-
-void TemperatureAndHumidityBegin(int pin)
-{
-  TemperatureAndHumidityPin = pin;
-  DHT11Init(TemperatureAndHumidityPin);
-}
-
-bool TemperatureAndHumidityRead(float* temperature, float* humidity)
-{
-  byte data[5];
-  
-  DHT11Start(TemperatureAndHumidityPin);
-  for (int i = 0; i < 5; i++) data[i] = DHT11ReadByte(TemperatureAndHumidityPin);
-  DHT11Finish(TemperatureAndHumidityPin);
-  
-  if(!DHT11Check(data, sizeof (data))) return false;
-  if (data[1] >= 10) return false;
-  if (data[3] >= 10) return false;
-
-  *humidity = (float)data[0] + (float)data[1] / 10.0f;
-  *temperature = (float)data[2] + (float)data[3] / 10.0f;
-
-  return true;
-}
-
-////////////////////////////////////////////////////////////////////////////////////////
-//
-
-void DHT11Init(int pin)
-{
-  digitalWrite(pin, HIGH);
-  pinMode(pin, OUTPUT);
-}
-
-void DHT11Start(int pin)
-{
-  // Host the start of signal
-  digitalWrite(pin, LOW);
-  delay(18);
-  
-  // Pulled up to wait for
-  pinMode(pin, INPUT);
-  while (!digitalRead(pin)) ;
-  
-  // Response signal
-  while (digitalRead(pin)) ;
-  
-  // Pulled ready to output
-  while (!digitalRead(pin)) ;
-}
-
-byte DHT11ReadByte(int pin)
-{
-  byte data = 0;
-  
-  for (int i = 0; i < 8; i++) {
-    while (digitalRead(pin)) ;
-
-    while (!digitalRead(pin)) ;
-    unsigned long start = micros();
-
-    while (digitalRead(pin)) ;
-    unsigned long finish = micros();
-
-    if ((unsigned long)(finish - start) > 50) data |= 1 << (7 - i);
-  }
-  
-  return data;
-}
-
-void DHT11Finish(int pin)
-{
-  // Releases the bus
-  while (!digitalRead(pin)) ;
-  digitalWrite(pin, HIGH);
-  pinMode(pin, OUTPUT);
-}
-
-bool DHT11Check(const byte* data, int dataSize)
-{
-  if (dataSize != 5) return false;
-
-  byte sum = 0;
-  for (int i = 0; i < dataSize - 1; i++) {
-    sum += data[i];
-  }
-
-  return data[dataSize - 1] == sum;
-}
-
-////////////////////////////////////////////////////////////////////////////////////////
