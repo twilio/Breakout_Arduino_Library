382abd2f2f9b8c1d0fe258e1c2207cd93fd06efe
Randy Beiter <rbeiter@twilio.com>
2018-11-05 16:46:50 -0500

    Update GNSS example

--- examples/Sensors/GPS/GPS.ino
+++ examples/Sensors/GPS/GPS.ino
@@ -1,18 +1,75 @@
 #include <board.h>
 #include <BreakoutSDK.h>
-#include <WioLTE_Cat_NB1/ublox_sara_r4_gnss.h>
+#include <stdio.h>
 
+/** Change this to your device purpose */
+static const char *device_purpose = "Dev-Kit";
+/** Change this to your key for the SIM card inserted in this device 
+ *  You can find your PSK under the Breakout SDK tab of your Narrowband SIM detail at
+ *  https://www.twilio.com/console/wireless/sims
+ */
+static const char *psk_key = "00112233445566778899aabbccddeeff";
 
-UBLOX_SARA_R4_GNSS gnss = UBLOX_SARA_R4_GNSS();
+/** This is the Breakout SDK top API */
+Breakout *breakout = &Breakout::getInstance();
 
-void setup()  
-{
-  // Open GNSS module
-  gnss.open_GNSS();
-  delay(3000);
-  SerialDebugPort.println("_Start");
+#define LOOP_INTERVAL (1 * 1000)
+#define SEND_INTERVAL (10 * 60 * 1000)
+
+void setup() {
+  // Feel free to change the log verbosity. E.g. from most critical to most verbose:
+  //   - errors: L_ALERT, L_CRIT, L_ERR, L_ISSUE
+  //   - warnings: L_WARN, L_NOTICE
+  //   - information & debug: L_INFO, L_DB, L_DBG, L_MEM
+  // When logging, the additional L_CLI level ensure that the output will always be visible, no matter the set level.
+  owl_log_set_level(L_INFO);
+  LOG(L_WARN, "Arduino setup() starting up\r\n");
+
+  // Set the Breakout SDK parameters
+  breakout->setPurpose(device_purpose);
+  breakout->setPSKKey(psk_key);
+  breakout->setPollingInterval(10 * 60);  // Optional, by default set to 10 minutes
+
+  // Powering the modem and starting up the SDK
+  LOG(L_WARN, "Powering on module and registering...");
+  breakout->powerModuleOn();
+
+  LOG(L_WARN, "... done powering on and registering.\r\n");
+  LOG(L_WARN, "Arduino loop() starting up\r\n");
 }
 
-void loop() {
-  gnss.dataFlowMode();
+/**
+ * This is just a simple example to send a command and write out the status to the console.
+ */
+
+void sendCommand(const char * command) {
+  if (breakout->sendTextCommand(command) == COMMAND_STATUS_OK) {
+    LOG(L_INFO, "Tx-Command [%s]\r\n", command);
+  } else {
+    LOG(L_INFO, "Tx-Command ERROR\r\n");
+  }
+}
+
+void loop()
+{
+  static unsigned long last_send = 0;
+
+  gnss_data_t data;
+  breakout->getGNSSData(&data);
+
+  if (data.valid && ((last_send == 0) || (millis() - last_send >= SEND_INTERVAL))) {
+    last_send = millis();
+
+    if (data.valid) {
+      char commandText[512];
+      snprintf(commandText, 512, "Current Position:  %d %7.5f %s  %d %7.5f %s\r\n", data.position.latitude_degrees,
+          data.position.latitude_minutes, data.position.is_north ? "N" : "S", data.position.longitude_degrees,
+          data.position.longitude_minutes, data.position.is_west ? "W" : "E");
+      sendCommand(commandText);
+    }
+  }
+
+  breakout->spin();
+
+  delay(LOOP_INTERVAL);
 }
